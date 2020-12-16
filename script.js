9a49e71c884e7451935bf370bc37aed6277fa19f
Federica Buzzi <infofedericabuzzi@gmail.com>
2020-12-17 00:01:32 +0100

    latest

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function render() {
   for (let i = 0; i < affiches.length; i++) {
     affiches[i].addEventListener('click', () => {
       const popupDiv = document.getElementById('popup');
-      popupDiv.innerHTML = `<div class="insidePopup"><h2>Title : ${movies[i].title}</h2><br>
+      popupDiv.innerHTML = `<div id="insidePopup"><h2>Title : ${movies[i].title}</h2><br>
     Genres: ${movies[i].genres}<br>
     Year: ${movies[i].year}<br>
     Note: ${movies[i].note}<br>
@@ -33,26 +33,26 @@ function render() {
 
   // fermeture popup - button still not working
 
-  document.body.addEventListener('click', (e) => {
-    // const popupCache = document.('insidePopup');
-    // if (e.target.matches('#fermerButton')) {
-    //   console.log('dommage');
-    //   popupCache.display.style = 'none';
-    // } else {
-    //   popupCache.display.style = 'none';
-    // }
+  // document.body.addEventListener('click', (e) => {
+  // const popupCache = document.('insidePopup');
+  // if (e.target.matches('#fermerButton')) {
+  //   console.log('dommage');
+  //   popupCache.display.style = 'none';
+  // } else {
+  //   popupCache.display.style = 'none';
+  // }
 
-    // seulements films après 2000
-    if (e.target.matches('#recentButton')) {
-      for (let i = 0; i < movies.length; i++) {
-        if (movies[i].year < 2000) {
-          console.log(movies[i].title);
-          const x = document.querySelectorAll('.insidePopup');
-          console.log(x);
-          // [i].style.display = 'none';
-        }
-      }
-    }
-  });
+  // seulements films après 2000
+  // if (e.target.matches('#recentButton')) {
+  //   for (let i = 0; i < movies.length; i++) {
+  //     if (movies[i].year < 2000) {
+  //       console.log(movies[i].title);
+  //       const x = document.querySelectorAll('.insidePopup');
+  //       console.log(x);
+  // [i].style.display = 'none';
+  //       }
+  //     }
+  //   }
+  // });
 }
 render();
